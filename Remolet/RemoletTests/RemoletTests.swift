2a7cdc3f610c6e4de69f6e07e81cdbe22e1cf79d
liweiz <matt.z.lw@gmail.com>
2014-11-30 15:47:22 -0500

    added swift rewrite project

---
+++ Remolet/RemoletTests/RemoletTests.swift
@@ -0,0 +1,36 @@
+//
+//  RemoletTests.swift
+//  RemoletTests
+//
+//  Created by Liwei Zhang on 2014-11-30.
+//  Copyright (c) 2014 Liwei Zhang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RemoletTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
