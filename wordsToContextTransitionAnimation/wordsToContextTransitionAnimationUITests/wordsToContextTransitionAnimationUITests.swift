41706202ef4b83452a098183145e0eabed59227b
liweiz <matt.z.lw@gmail.com>
2015-08-06 00:20:25 -0400

    WIP

---
+++ wordsToContextTransitionAnimation/wordsToContextTransitionAnimationUITests/wordsToContextTransitionAnimationUITests.swift
@@ -0,0 +1,34 @@
+//
+//  wordsToContextTransitionAnimationUITests.swift
+//  wordsToContextTransitionAnimationUITests
+//
+//  Created by Liwei Zhang on 2015-08-06.
+//  Copyright © 2015 Liwei Zhang. All rights reserved.
+//
+
+import XCTest
+
+class wordsToContextTransitionAnimationUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
